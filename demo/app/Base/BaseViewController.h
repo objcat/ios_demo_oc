8a829f809a8c3cf7119e2f0d7cd0071c66feda3e
objcat <objcat@163.com>
2020-06-12 17:19:07 +0800

    完善注释

--- demo/app/Base/BaseViewController.h
+++ demo/app/Base/BaseViewController.h
@@ -20,11 +20,15 @@
 
 /**
  * 设置返回按钮的标题
+ * 支持动态修改
+ * 需要在前一个页面进行修改
  */
 @property (strong, nonatomic) NSString *backButtonTitle;
 
 /**
  * 设置返回按钮文字和按钮颜色
+ * 支持动态修改
+ * 需要在前一个页面进行修改
  */
 @property (strong, nonatomic) UIColor *backButtonTintColor;
 
